1b4563e4165e369611d5d67d5c4cc1ce450dfb67
SakshiAg02 <113249274+SakshiAg02@users.noreply.github.com>
2022-11-19 18:56:06 +0530

    Add files via upload

---
+++ project 101.py
@@ -0,0 +1,42 @@
+import random
+response = "Y"
+while response == "Y":
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    if no == 2:
+        print("[-----]")
+        print("[     ]")
+        print("[  00 ]")
+        print("[     ]")
+        print("[-----]")
+    if no == 3:
+        print("[-----]")
+        print("[     ]")
+        print("[ 000 ]")
+        print("[     ]")
+        print("[-----]")
+    if no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+    response = input("do you want to roll a dice yes(Y)or no(N)")
+    print("\n")
